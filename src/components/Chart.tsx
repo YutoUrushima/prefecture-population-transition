d4cbe425013a1f1847646ed9f9f86de2705faa7e
yuto urushima <yuto259233@gmail.com>
2021-08-17 22:05:54 +0900

    apiからのデータをグラフに反映

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,34 +1,70 @@
 import { Line } from "react-chartjs-2";
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import axios from "axios";
 
-// const url =
-//   "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=31";
+const url =
+  "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=31";
 
-// const apiKey = process.env.REACT_APP_APIKEY ? process.env.REACT_APP_APIKEY : "";
+const apiKey = process.env.REACT_APP_APIKEY ? process.env.REACT_APP_APIKEY : "";
 
 const Chart = () => {
   const [chartData, setChartData] = useState({});
   const templateData = () => {
-    setChartData({
-      labels: ["1970", "1980", "1990", "2000", "2010", "2020"],
-      datasets: [
-        {
-          label: "population",
-          data: [1, 2, 3, 4, 5, 6],
-          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-          borderColor: ["rgba(255, 99, 132, 1)"],
-          borderWidth: 1
+    let empPopulation: number[] = [];
+    axios
+      .get(url, {
+        headers: {
+          "X-API-KEY": apiKey
         }
-      ]
-    });
+      })
+      .then((res) => {
+        const fetchedData = res.data.result.data[0].data;
+        empPopulation.push(fetchedData[2].value);
+        empPopulation.push(fetchedData[4].value);
+        empPopulation.push(fetchedData[6].value);
+        empPopulation.push(fetchedData[8].value);
+        empPopulation.push(fetchedData[10].value);
+        empPopulation.push(fetchedData[12].value);
+        // console.log(empPopulation);
+        setChartData({
+          labels: ["1970", "1980", "1990", "2000", "2010", "2020"],
+          datasets: [
+            {
+              label: "population",
+              data: empPopulation,
+              backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+              borderColor: ["rgba(255, 99, 132, 1)"],
+              borderWidth: 2
+            }
+          ]
+        });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   };
   useEffect(() => {
     templateData();
   }, []);
   return (
     <div>
-      <Line data={chartData} />
+      <Line
+        data={chartData}
+        options={{
+          responsive: true,
+          title: {
+            text: "THICKNESS SCALE",
+            display: true,
+            scales: {
+              yAxes: {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            }
+          }
+        }}
+      />
     </div>
   );
 };
