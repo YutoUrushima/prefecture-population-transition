c460d150e055bab39f06b52b7c8a86f5f6113e46
yuto urushima <yuto259233@gmail.com>
2021-08-17 20:46:42 +0900

    y軸のオプション追加 dataの格納

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -5,6 +5,7 @@ const url =
   "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=31";
 
 const apiKey = process.env.REACT_APP_APIKEY ? process.env.REACT_APP_APIKEY : "";
+const displayData: number[] = [];
 
 const Chart = () => {
   const getData = async () => {
@@ -23,38 +24,48 @@ const Chart = () => {
   };
   getData()
     .then((data) => {
-      console.log(JSON.stringify(data));
+      const eachPopilation = data.result.data[0].data;
+      displayData.push(eachPopilation[2].value);
+      displayData.push(eachPopilation[4].value);
+      displayData.push(eachPopilation[6].value);
+      displayData.push(eachPopilation[8].value);
+      displayData.push(eachPopilation[10].value);
+      displayData.push(eachPopilation[12].value);
+      // console.log(displayData);
     })
     .catch((err) => {
       console.log(err.message);
     });
   const data = {
-    labels: ["1970", "1980", "1990", "2000", "2010", "2020"],
+    labels: [
+      "1970",
+      "1980",
+      "1990",
+      "2000",
+      "2010",
+      "2020",
+      "1970",
+      "1980",
+      "1990",
+      "2000",
+      "2010",
+      "2020"
+    ],
     datasets: [
       {
         label: "Population Transition",
-        data: [12, 19, 3, 5, 2, 3],
+        data: displayData,
+        // data: [12, 19, 3, 5, 2, 3],
         backgroundColor: ["rgba(255, 99, 132, 1)"],
         borderColor: ["rgba(255, 99, 132, 1)"],
         borderWidth: 1
       }
     ]
   };
-
-  const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
-          }
-        }
-      ]
-    }
-  };
+  console.log(data.datasets[0].data);
   return (
     <div>
-      <Line data={data} width={100} height={50} options={options} />
+      <Line data={data} width={100} height={50} />
     </div>
   );
 };
