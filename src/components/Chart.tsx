0b439519d4723810915f805aac8fb2063a0d9f1b
yuto urushima <yuto259233@gmail.com>
2021-08-17 21:04:03 +0900

    axiosを使用したロジックの書き換え

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,6 @@
 import { Line } from "react-chartjs-2";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const url =
   "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=31";
@@ -7,54 +8,68 @@ const url =
 const apiKey = process.env.REACT_APP_APIKEY ? process.env.REACT_APP_APIKEY : "";
 
 const Chart = () => {
-  const getData = async () => {
-    try {
-      const res = await fetch(url, {
-        method: "GET",
-        headers: {
-          "X-API-KEY": apiKey
+  const [dataChart, setDataChart] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      let confirmedCases: any = [];
+      let dateOfCases: any = [];
+      await axios.get(`${url}?access_key=${apiKey}`).then((res) => {
+        for (let dataObj of res.data) {
+          confirmedCases.push(parseInt(dataObj.Cases));
+          let tempDate = new Date(dataObj.Date);
+          dateOfCases.push(tempDate.getUTCMonth() + 1 + "/" + tempDate.getUTCDate());
         }
       });
-      const json = await res.json();
-      return json;
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  getData()
-    .then((data) => {
-      console.log(JSON.stringify(data));
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  const data = {
-    labels: ["1970", "1980", "1990", "2000", "2010", "2020"],
-    datasets: [
-      {
-        label: "Population Transition",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: ["rgba(255, 99, 132, 1)"],
-        borderColor: ["rgba(255, 99, 132, 1)"],
-        borderWidth: 1
-      }
-    ]
-  };
+      setDataChart({
+        labels: dateOfCases, //x軸ラベル
+        datasets: [
+          {
+            label: "新型コロナウイルス感染者数の推移（日本）", //y軸のラベル
+            backgroundColor: "rgba(75,192,192,0.4)", //グラフの色
+            borderColor: "rgba(75,192,192)", //
+            borderWidth: 1,
+            data: confirmedCases //y軸ラベル
+          }
+        ]
+      });
+    };
+    fetchData();
+  }, []);
 
   const options = {
+    legend: {
+      display: true //y軸のラベル表示
+    },
     scales: {
       yAxes: [
         {
           ticks: {
-            beginAtZero: true
+            max: 26000,
+            min: 0
+            //stepSize: 3
+          }
+        }
+      ],
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            unit: "month",
+            displayFormats: {
+              quarter: "MMM YYYY"
+            }
           }
         }
       ]
+    },
+    title: {
+      display: true, //グラフのタイトル表示
+      text: ["グラフの", "タイトル"] //グラフのタイトルは改行もできる
     }
   };
   return (
     <div>
-      <Line data={data} width={100} height={50} options={options} />
+      <Line data={dataChart} options={options} />
     </div>
   );
 };
