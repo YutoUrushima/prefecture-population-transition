7a593c0b587b849212b3b35f8946135100c2339f
yuto urushima <yuto259233@gmail.com>
2021-08-18 22:12:33 +0900

    単数選択時のstateによる都道府県の切り替えとグラフへの反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.scss";
 import Chart from "./components/Chart";
 
 const App: React.FC = () => {
+  const [pref, setPref] = useState<number>(0);
+  const handlePrefCheck = (number: number) => {
+    setPref(number);
+  };
   return (
     <div className={styles.app}>
-      <Chart />
+      <label className={styles.app_check}>
+        <input
+          type="radio"
+          name="pref"
+          value=""
+          onChange={() => {
+            handlePrefCheck(1);
+          }}
+        />
+        <p>北海道</p>
+      </label>
+      <label className={styles.app_check}>
+        <input
+          type="radio"
+          name="pref"
+          value=""
+          onChange={() => {
+            handlePrefCheck(2);
+          }}
+        />
+        <p>青森県</p>
+      </label>
+      <label className={styles.app_check}>
+        <input
+          type="radio"
+          name="pref"
+          value=""
+          onChange={() => {
+            handlePrefCheck(3);
+          }}
+        />
+        <p>岩手県</p>
+      </label>
+      <Chart pref={pref} />
     </div>
   );
 };
