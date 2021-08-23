39f0dfcb5d80a4fa923499c36b6581af530d7816
yuto urushima <yuto259233@gmail.com>
2021-08-23 19:52:10 +0900

    都道府県を配列に格納してmapで表示

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import styles from "./App.module.scss";
 import Chart from "./components/Chart";
 
+const prefArray = [
+  { number: 1, name: "北海道" },
+  { number: 2, name: "青森県" },
+  { number: 3, name: "岩手" }
+];
+
 const App: React.FC = () => {
   const [pref, setPref] = useState<number>(0);
   const [name, setName] = useState<string>("");
@@ -11,39 +17,21 @@ const App: React.FC = () => {
   };
   return (
     <div className={styles.app}>
-      <label className={styles.app_check}>
-        <input
-          type="radio"
-          name="pref"
-          value=""
-          onChange={() => {
-            handlePrefCheck(1, "北海道");
-          }}
-        />
-        <p>北海道</p>
-      </label>
-      <label className={styles.app_check}>
-        <input
-          type="radio"
-          name="pref"
-          value=""
-          onChange={() => {
-            handlePrefCheck(2, "青森県");
-          }}
-        />
-        <p>青森県</p>
-      </label>
-      <label className={styles.app_check}>
-        <input
-          type="radio"
-          name="pref"
-          value=""
-          onChange={() => {
-            handlePrefCheck(3, "岩手県");
-          }}
-        />
-        <p>岩手県</p>
-      </label>
+      {prefArray.map((pref) => {
+        return (
+          <label key={pref.number} className={styles.app_check}>
+            <input
+              type="radio"
+              name="pref"
+              value=""
+              onChange={() => {
+                handlePrefCheck(pref.number, pref.name);
+              }}
+            />
+            <p>{pref.name}</p>
+          </label>
+        );
+      })}
       <Chart pref={pref} name={name} />
     </div>
   );
