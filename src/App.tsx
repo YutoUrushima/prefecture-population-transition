21212bf3056b8db4ac1063d5795f6fe5e77793f9
yuto urushima <yuto259233@gmail.com>
2021-08-29 18:00:10 +0900

    全都道府県の追加 & スタイルの調整

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,194 @@ import styles from "./App.module.scss";
 import Chart from "./components/Chart";
 
 const prefArray = [
-  { number: 1, name: "北海道" },
-  { number: 2, name: "青森県" },
-  { number: 3, name: "岩手" }
+  {
+    prefCode: 1,
+    prefName: "北海道"
+  },
+  {
+    prefCode: 2,
+    prefName: "青森県"
+  },
+  {
+    prefCode: 3,
+    prefName: "岩手県"
+  },
+  {
+    prefCode: 4,
+    prefName: "宮城県"
+  },
+  {
+    prefCode: 5,
+    prefName: "秋田県"
+  },
+  {
+    prefCode: 6,
+    prefName: "山形県"
+  },
+  {
+    prefCode: 7,
+    prefName: "福島県"
+  },
+  {
+    prefCode: 8,
+    prefName: "茨城県"
+  },
+  {
+    prefCode: 9,
+    prefName: "栃木県"
+  },
+  {
+    prefCode: 10,
+    prefName: "群馬県"
+  },
+  {
+    prefCode: 11,
+    prefName: "埼玉県"
+  },
+  {
+    prefCode: 12,
+    prefName: "千葉県"
+  },
+  {
+    prefCode: 13,
+    prefName: "東京都"
+  },
+  {
+    prefCode: 14,
+    prefName: "神奈川県"
+  },
+  {
+    prefCode: 15,
+    prefName: "新潟県"
+  },
+  {
+    prefCode: 16,
+    prefName: "富山県"
+  },
+  {
+    prefCode: 17,
+    prefName: "石川県"
+  },
+  {
+    prefCode: 18,
+    prefName: "福井県"
+  },
+  {
+    prefCode: 19,
+    prefName: "山梨県"
+  },
+  {
+    prefCode: 20,
+    prefName: "長野県"
+  },
+  {
+    prefCode: 21,
+    prefName: "岐阜県"
+  },
+  {
+    prefCode: 22,
+    prefName: "静岡県"
+  },
+  {
+    prefCode: 23,
+    prefName: "愛知県"
+  },
+  {
+    prefCode: 24,
+    prefName: "三重県"
+  },
+  {
+    prefCode: 25,
+    prefName: "滋賀県"
+  },
+  {
+    prefCode: 26,
+    prefName: "京都府"
+  },
+  {
+    prefCode: 27,
+    prefName: "大阪府"
+  },
+  {
+    prefCode: 28,
+    prefName: "兵庫県"
+  },
+  {
+    prefCode: 29,
+    prefName: "奈良県"
+  },
+  {
+    prefCode: 30,
+    prefName: "和歌山県"
+  },
+  {
+    prefCode: 31,
+    prefName: "鳥取県"
+  },
+  {
+    prefCode: 32,
+    prefName: "島根県"
+  },
+  {
+    prefCode: 33,
+    prefName: "岡山県"
+  },
+  {
+    prefCode: 34,
+    prefName: "広島県"
+  },
+  {
+    prefCode: 35,
+    prefName: "山口県"
+  },
+  {
+    prefCode: 36,
+    prefName: "徳島県"
+  },
+  {
+    prefCode: 37,
+    prefName: "香川県"
+  },
+  {
+    prefCode: 38,
+    prefName: "愛媛県"
+  },
+  {
+    prefCode: 39,
+    prefName: "高知県"
+  },
+  {
+    prefCode: 40,
+    prefName: "福岡県"
+  },
+  {
+    prefCode: 41,
+    prefName: "佐賀県"
+  },
+  {
+    prefCode: 42,
+    prefName: "長崎県"
+  },
+  {
+    prefCode: 43,
+    prefName: "熊本県"
+  },
+  {
+    prefCode: 44,
+    prefName: "大分県"
+  },
+  {
+    prefCode: 45,
+    prefName: "宮崎県"
+  },
+  {
+    prefCode: 46,
+    prefName: "鹿児島県"
+  },
+  {
+    prefCode: 47,
+    prefName: "沖縄県"
+  }
 ];
 
 const App: React.FC = () => {
@@ -17,21 +202,24 @@ const App: React.FC = () => {
   };
   return (
     <div className={styles.app}>
-      {prefArray.map((pref) => {
-        return (
-          <label key={pref.number} className={styles.app_check}>
-            <input
-              type="radio"
-              name="pref"
-              value=""
-              onChange={() => {
-                handlePrefCheck(pref.number, pref.name);
-              }}
-            />
-            <p>{pref.name}</p>
-          </label>
-        );
-      })}
+      <h1 className={styles.app_title}>Prefecture Population Transition</h1>
+      <div className={styles.app_list}>
+        {prefArray.map((pref) => {
+          return (
+            <label key={pref.prefCode} className={styles.app_check}>
+              <input
+                type="radio"
+                name="pref"
+                value=""
+                onChange={() => {
+                  handlePrefCheck(pref.prefCode, pref.prefName);
+                }}
+              />
+              <p>{pref.prefName}</p>
+            </label>
+          );
+        })}
+      </div>
       <Chart pref={pref} name={name} />
     </div>
   );
